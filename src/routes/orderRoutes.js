49fb098f39472084673387a7e857c4f297790f80
kandelmilan <kandelr950p@gmail.com>
2026-04-19 17:27:24 +0545

    Added

--- src/routes/orderRoutes.js
+++ src/routes/orderRoutes.js
@@ -2,10 +2,12 @@ const express = require("express");
 const router = express.Router();
 const orderController = require("../controllers/orderController");
 
-// GET all orders
 router.get("/", orderController.getOrders);
+
 router.post("/", orderController.createOrder);
+
 router.put("/:id", orderController.updateOrder);
+
 router.delete("/:id", orderController.deleteOrder);
 
 module.exports = router;
